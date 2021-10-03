dec753e8eca19f1e1c8ac5bdf502d99815ae5a0f
Dmitry Landa <dmitry.landa@yandex.ru>
2021-10-03 12:40:08 +0300

    Add expense parser to accept data as user friendly string

---
+++ api/src/Services/ExpenseParser.php
@@ -0,0 +1,151 @@
+<?php
+
+namespace App\Services;
+
+use App\Entity\Expense;
+use DateTime;
+use DateTimeInterface;
+use Exception;
+use Throwable;
+
+class ExpenseParser
+{
+    private const MONTHES = [
+        'января' => 'january',
+        'февраля' => 'february',
+        'марта' => 'march',
+        'апреля' => 'april',
+        'мая' => 'may',
+        'июня' => 'june',
+        'июля' => 'july',
+        'августа' => 'august',
+        'сентября' => 'september',
+        'октября' => 'october',
+        'ноября' => 'november',
+        'декабря' => 'december',
+    ];
+
+    private const WEEK_DAYS = [
+        'сегодня' => 'now',
+        'вчера' => '-1 day',
+        'понедельник' => 'last monday',
+        'вторник' => 'last tuesday',
+        'среда' => 'last wednesday',
+        'четверг' => 'last thursday',
+        'пятница' => 'last friday',
+        'суббота' => 'last saturday',
+        'воскресенье' => 'last sunday',
+    ];
+
+    public function parse(string $expression): Expense
+    {
+        $expression = trim(strtolower($expression));
+
+        $tokens = $this->tokenize($expression);
+        [$value, $category, $date] = $this->normalize($tokens);
+
+        $this->validateValue($value);
+        $this->validateCategory($category);
+        $this->validateDate($date);
+
+        return (new Expense())
+            ->setValue($value)
+            ->setCategory($category)
+            ->setCreatedAt($date)
+        ;
+    }
+
+    private function tokenize(string $expression): array
+    {
+        return explode(' ', $expression);
+    }
+
+    private function normalize(array $tokens)
+    {
+        $tokensCount = count($tokens);
+        switch (true) {
+            case $tokensCount < 2:
+                throw new \Exception('bad input');
+            case $tokensCount === 2:
+                $value = $tokens[0];
+                $category = $tokens[1];
+                $date = new DateTime();
+                break;
+            default:
+                $value = $tokens[0];
+
+                if ($this->isMonthName($tokens[2])) {
+                    $date = $this->parseDate("{$tokens[1]} {$tokens[2]}");
+                    $category = $tokens[3] ?? null;
+                } else {
+                    $date = $this->parseDate($tokens[1]);
+                    $category = $tokens[2];
+                }
+
+                break;
+        }
+
+        return [$value, $category, $date];
+    }
+
+    private function isMonthName(string $string): bool
+    {
+        return array_key_exists($string, self::MONTHES);
+    }
+
+    private function validateValue(string $value): bool
+    {
+        if (preg_match('/^(\-?\d+(\.\d+)?)$/', $value)) {
+            return true;
+        }
+
+        throw new Exception('wrong value');
+    }
+
+    private function validateCategory(?string $value): bool
+    {
+        if (strlen($value) > 0) {
+            return true;
+        }
+
+        throw new Exception('wrong category');
+    }
+
+    private function validateDate(DateTimeInterface $date): bool
+    {
+        $now = new DateTime();
+
+        if ($now->getTimestamp() >= $date->getTimestamp()) {
+            return true;
+        }
+
+        throw new Exception('date should be in past');
+    }
+
+    private function parseDate(string $string): DateTimeInterface
+    {
+        $value = $string;
+        if (array_key_exists($string, self::WEEK_DAYS)) {
+            $value = self::WEEK_DAYS[$string];
+        }
+
+        if (preg_match('/^(\d){4}\-(\d){2}\-(\d){2}$/', $string)) {
+            $value = $string;
+        }
+
+        $tokens = explode(' ', $string);
+        if (count($tokens) === 2 && $this->isMonthName($tokens[1])) {
+            $value = $tokens[0];
+            $value .= ' ';
+            $value .= self::MONTHES[$tokens[1]];
+            $value .= ' ';
+            $value .= (new DateTime())->format('Y');
+        }
+
+        try {
+            return new DateTime($value);
+        } catch (Throwable $e) {
+            throw new \Exception('wrong date');
+        }
+    }
+}
---
+++ api/tests/Service/ExpenseParserTest.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Tests\Service;
+
+use App\Entity\Expense;
+use App\Services\ExpenseParser;
+use DateTime;
+use PHPUnit\Framework\TestCase;
+
+class ExpenseParserTest extends TestCase
+{
+    /**
+     * test
+     * @dataProvider expenseParserDataProvider
+     */
+    public function itParsesExpressionsInDifferentFormats(string $expression, array $expectations): void
+    {
+        $service = new ExpenseParser();
+
+        $expense = $service->parse($expression);
+
+        [$value, $category, $date] = $expectations;
+
+        self::assertInstanceOf(Expense::class, $expense);
+        self::assertEquals($value, $expense->getValue());
+        self::assertEquals($category, $expense->getCategory());
+        self::assertEquals($date->format('Y-m-d'), $expense->getCreatedAt()->format('Y-m-d'));
+    }
+
+    public function expenseParserDataProvider(): array
+    {
+        return [
+            ['-1500 вчера бар', [-1500, 'бар', new DateTime('-1 day')]],
+            ['-137.50 обед', [-137.5, 'обед', new DateTime()]],
+            ['300 23 сентября кино', [300, 'кино', new DateTime('23 september')]],
+            ['-1500 среда бар', [-1500, 'бар', new DateTime('last wednesday')]],
+            ['-100 воскресенье бар', [-100, 'бар', new DateTime('last sunday')]],
+            ['-1050 2020-03-15 бар', [-1050, 'бар', new DateTime('2020-03-15')]],
+        ];
+    }
+
+    /**
+     * @test
+     * @dataProvider badInputDataProvider
+     */
+    public function itThrowsExceptionOnFailure(string $expression): void
+    {
+        self::expectException(\Exception::class);
+
+        $service = new ExpenseParser();
+        $service->parse($expression);
+    }
+
+    public function badInputDataProvider(): array
+    {
+        return [
+            ['-foo вчера бар'],
+            ['-137.50.45 обед'],
+            ['300 20 хренобря кино'],
+            ['-1500'],
+            ['-100 23 сентября'],
+            ['-1050 завтра бар'],
+        ];
+    }
+}
